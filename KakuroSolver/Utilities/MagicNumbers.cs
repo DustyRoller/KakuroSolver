679fc2f4838adfea2a49ad0c4ae85f3fa8fb5249
DustyRoller <DustyRoller@github.com>
2022-03-10 14:31:56 +0000

    Made the MagicNumber values immutable

--- KakuroSolver/Utilities/MagicNumbers.cs
+++ KakuroSolver/Utilities/MagicNumbers.cs
@@ -4,9 +4,46 @@ namespace KakuroSolver.Utilities
 {
     class IntegerPartitions
     {
-        public uint PartitionLength;
-        public uint Total;
-        public List<List<uint>> Values;
+        /// <summary>
+        /// Gets the length of partition.
+        /// </summary>
+        public uint PartitionLength { get; private set; }
+
+        /// <summary>
+        /// Gets the total value of the partition.
+        /// </summary>
+        public uint Total { get; private set; }
+
+        /// <summary>
+        /// Gets a list of all of the valid integer partitions combinations.
+        /// </summary>
+        /// <remarks>
+        /// The list returned is a deep copy of the original allowing the user
+        /// to modifiy the list as required.
+        /// </remarks>
+        public List<List<uint>> Values
+        {
+            get { return values.ConvertAll(v => new List<uint>(v)); }
+            private set { values = value; }
+        }
+
+        /// <summary>
+        /// Underlying integer partition combinations values.
+        /// </summary>
+        private List<List<uint>> values;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="partitionLength">The length of the partition.</param>
+        /// <param name="total">The total value of the partition.</param>
+        /// <param name="values">List of the valid integer partition combinations.</param>
+        public IntegerPartitions(uint partitionLength, uint total, List<List<uint>> values)
+        {
+            PartitionLength = partitionLength;
+            Total = total;
+            this.values = values;
+        }
     }
 
     sealed class MagicNumbers
@@ -16,40 +53,40 @@ namespace KakuroSolver.Utilities
         /// </summary>
         public static readonly List<IntegerPartitions> MagicNumberValues = new()
         {
-            new IntegerPartitions { PartitionLength = 2, Total = 3, Values = new List<List<uint>>() { new List<uint> { 1, 2, } } },
-            new IntegerPartitions { PartitionLength = 2, Total = 4, Values = new List<List<uint>>() { new List<uint> { 1, 3, } } },
-            new IntegerPartitions { PartitionLength = 2, Total = 16, Values = new List<List<uint>>() { new List<uint> { 7, 9, } } },
-            new IntegerPartitions { PartitionLength = 2, Total = 17, Values = new List<List<uint>>() { new List<uint> { 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 3, Total = 6, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, } } },
-            new IntegerPartitions { PartitionLength = 3, Total = 7, Values = new List<List<uint>>() { new List<uint> { 1, 2, 4, } } },
-            new IntegerPartitions { PartitionLength = 3, Total = 23, Values = new List<List<uint>>() { new List<uint> { 6, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 3, Total = 24, Values = new List<List<uint>>() { new List<uint> { 7, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 4, Total = 10, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, } } },
-            new IntegerPartitions { PartitionLength = 4, Total = 11, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, 5, } } },
-            new IntegerPartitions { PartitionLength = 4, Total = 29, Values = new List<List<uint>>() { new List<uint> { 5, 7, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 4, Total = 30, Values = new List<List<uint>>() { new List<uint> { 6, 7, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 5, Total = 15, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, } } },
-            new IntegerPartitions { PartitionLength = 5, Total = 16, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 6, } } },
-            new IntegerPartitions { PartitionLength = 5, Total = 34, Values = new List<List<uint>>() { new List<uint> { 4, 6, 7, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 5, Total = 35, Values = new List<List<uint>>() { new List<uint> { 5, 6, 7, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 6, Total = 21, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 6, } } },
-            new IntegerPartitions { PartitionLength = 6, Total = 22, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 7, } } },
-            new IntegerPartitions { PartitionLength = 6, Total = 38, Values = new List<List<uint>>() { new List<uint> { 3, 5, 6, 7, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 6, Total = 39, Values = new List<List<uint>>() { new List<uint> { 4, 5, 6, 7, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 7, Total = 28, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 6, 7, } } },
-            new IntegerPartitions { PartitionLength = 7, Total = 29, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 6, 8, } } },
-            new IntegerPartitions { PartitionLength = 7, Total = 41, Values = new List<List<uint>>() { new List<uint> { 2, 4, 5, 6, 7, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 7, Total = 42, Values = new List<List<uint>>() { new List<uint> { 3, 4, 5, 6, 7, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 8, Total = 36, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 6, 7, 8, } } },
-            new IntegerPartitions { PartitionLength = 8, Total = 37, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 6, 7, 9, } } },
-            new IntegerPartitions { PartitionLength = 8, Total = 38, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 6, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 8, Total = 39, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 7, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 8, Total = 40, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 6, 7, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 8, Total = 41, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, 5, 6, 7, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 8, Total = 42, Values = new List<List<uint>>() { new List<uint> { 1, 2, 4, 5, 6, 7, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 8, Total = 43, Values = new List<List<uint>>() { new List<uint> { 1, 3, 4, 5, 6, 7, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 8, Total = 44, Values = new List<List<uint>>() { new List<uint> { 2, 3, 4, 5, 6, 7, 8, 9, } } },
-            new IntegerPartitions { PartitionLength = 9, Total = 45, Values = new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 6, 7, 8, 9, } } },
+            new IntegerPartitions(2, 3, new List<List<uint>>() { new List<uint> { 1, 2, } }),
+            new IntegerPartitions(2, 4, new List<List<uint>>() { new List<uint> { 1, 3, } }),
+            new IntegerPartitions(2, 16, new List<List<uint>>() { new List<uint> { 7, 9, } }),
+            new IntegerPartitions(2, 17, new List<List<uint>>() { new List<uint> { 8, 9, } }),
+            new IntegerPartitions(3, 6, new List<List<uint>>() { new List<uint> { 1, 2, 3, } }),
+            new IntegerPartitions(3, 7, new List<List<uint>>() { new List<uint> { 1, 2, 4, } }),
+            new IntegerPartitions(3, 23, new List<List<uint>>() { new List<uint> { 6, 8, 9, } }),
+            new IntegerPartitions(3, 24, new List<List<uint>>() { new List<uint> { 7, 8, 9, } }),
+            new IntegerPartitions(4, 10, new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, } }),
+            new IntegerPartitions(4, 11, new List<List<uint>>() { new List<uint> { 1, 2, 3, 5, } }),
+            new IntegerPartitions(4, 29, new List<List<uint>>() { new List<uint> { 5, 7, 8, 9, } }),
+            new IntegerPartitions(4, 30, new List<List<uint>>() { new List<uint> { 6, 7, 8, 9, } }),
+            new IntegerPartitions(5, 15, new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, } }),
+            new IntegerPartitions(5, 16, new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 6, } }),
+            new IntegerPartitions(5, 34, new List<List<uint>>() { new List<uint> { 4, 6, 7, 8, 9, } }),
+            new IntegerPartitions(5, 35, new List<List<uint>>() { new List<uint> { 5, 6, 7, 8, 9, } }),
+            new IntegerPartitions(6, 21, new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 6, } }),
+            new IntegerPartitions(6, 22, new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 7, } }),
+            new IntegerPartitions(6, 38, new List<List<uint>>() { new List<uint> { 3, 5, 6, 7, 8, 9, } }),
+            new IntegerPartitions(6, 39, new List<List<uint>>() { new List<uint> { 4, 5, 6, 7, 8, 9, } }),
+            new IntegerPartitions(7, 28, new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 6, 7, } }),
+            new IntegerPartitions(7, 29, new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 6, 8, } }),
+            new IntegerPartitions(7, 41, new List<List<uint>>() { new List<uint> { 2, 4, 5, 6, 7, 8, 9, } }),
+            new IntegerPartitions(7, 42, new List<List<uint>>() { new List<uint> { 3, 4, 5, 6, 7, 8, 9, } }),
+            new IntegerPartitions(8, 36, new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 6, 7, 8, } }),
+            new IntegerPartitions(8, 37, new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 6, 7, 9, } }),
+            new IntegerPartitions(8, 38, new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 6, 8, 9, } }),
+            new IntegerPartitions(8, 39, new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 7, 8, 9, } }),
+            new IntegerPartitions(8, 40, new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 6, 7, 8, 9, } }),
+            new IntegerPartitions(8, 41, new List<List<uint>>() { new List<uint> { 1, 2, 3, 5, 6, 7, 8, 9, } }),
+            new IntegerPartitions(8, 42, new List<List<uint>>() { new List<uint> { 1, 2, 4, 5, 6, 7, 8, 9, } }),
+            new IntegerPartitions(8, 43, new List<List<uint>>() { new List<uint> { 1, 3, 4, 5, 6, 7, 8, 9, } }),
+            new IntegerPartitions(8, 44, new List<List<uint>>() { new List<uint> { 2, 3, 4, 5, 6, 7, 8, 9, } }),
+            new IntegerPartitions(9, 45, new List<List<uint>>() { new List<uint> { 1, 2, 3, 4, 5, 6, 7, 8, 9, } }),
         };
     }
 }
