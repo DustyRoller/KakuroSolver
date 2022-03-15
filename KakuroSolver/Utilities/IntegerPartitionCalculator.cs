008f555f3ea945d7041c7e16f9d3b04031189e75
DustyRoller <DustyRoller@github.com>
2022-03-15 10:27:34 +0000

    Added cache for IntegerPartitionCalculator

--- KakuroSolver/Utilities/IntegerPartitionCalculator.cs
+++ KakuroSolver/Utilities/IntegerPartitionCalculator.cs
@@ -7,47 +7,59 @@ namespace KakuroSolver.Utilities
     class IntegerPartitionCalculator
     {
         /// <summary>
+        /// As calculating integer partitions can be quite slow, keep a cache
+        /// of previously caclulated integer partitions and the known magic numbers.
+        /// </summary>
+        private static readonly List<IntegerPartitions> cache = new(MagicNumbers.MagicNumberValues);
+
+        /// <summary>
         /// Calculate the distinct integer partitionss for the given set
         /// of arguments.
         /// </summary>
-        /// <param name="sum">The sum that the partitions must add up to.</param>
+        /// <param name="total">The total that the partitions must add up to.</param>
         /// <param name="partitionLength">The number of partitions.</param>
-        /// <param name="minimumValue">The minimum value to be used in the partitions.</param>
-        /// <param name="maximumValue">The maximum value to be used in the partitions.</param>
+        /// <param name="minValue">The minimum value to be used in the partitions.</param>
+        /// <param name="maxValue">The maximum value to be used in the partitions.</param>
         /// <returns>A List of integer partitions.</returns>
-        public static List<List<uint>> CalculateDistinctIntegerPartitions(uint sum, uint partitionLength, uint minimumValue = 1, uint maximumValue = 9)
+        public static List<List<uint>> CalculateDistinctIntegerPartitions(uint total, uint partitionLength, uint minValue = 1, uint maxValue = 9)
         {
             // Provide some input validation before calling the recursive function.
-            if (maximumValue > 9)
+            if (maxValue > 9)
             {
                 throw new ArgumentException("Maximum value cannot be greater than 9.");
             }
 
-            if (maximumValue >= sum)
+            if (maxValue >= total)
             {
                 throw new ArgumentException("Maximum value cannot be greater than or equal to sum.");
             }
 
-            if (minimumValue >= maximumValue)
+            if (minValue >= maxValue)
             {
                 throw new ArgumentException("Minimum value must be less than the maximum value.");
             }
 
-            if (maximumValue <= minimumValue)
+            if (maxValue <= minValue)
             {
                 throw new ArgumentException("Maximum value must be greater than the minimum value.");
             }
 
-            // Check if the given parameters match those of a magic number.
-            var magicNumber = MagicNumbers.MagicNumberValues.FirstOrDefault(mn => mn.Total == sum &&
-                                                                            mn.PartitionLength == partitionLength &&
-                                                                            mn.Values[0].All(v => v >= minimumValue && v <= maximumValue));
+            var integerPartitions = cache.FirstOrDefault(ip => ip.PartitionLength == partitionLength &&
+                                                               ip.Total == total &&
+                                                               ip.Values[0].All(v => v >= minValue && v <= maxValue));
 
-            // If the value is a magic number then return those values,
-            // otherwise calculate the partitions.
-            return (magicNumber != null) ? magicNumber.Values
-                                         : CalculateDistinctIntegerPartitionsRecursive(sum, partitionLength,
-                                                                                       minimumValue, maximumValue);
+            if (integerPartitions == null)
+            {
+                // We need to calculate the integer partition.
+                var values = IntegerPartitionCalculator.CalculateDistinctIntegerPartitionsRecursive(total, partitionLength, minValue, maxValue);
+
+                integerPartitions = new IntegerPartitions(total, partitionLength, values);
+
+                // Now add it to the cache for future use.
+                cache.Add(integerPartitions);
+            }
+
+            return integerPartitions.Values;
         }
 
         /// <summary>
