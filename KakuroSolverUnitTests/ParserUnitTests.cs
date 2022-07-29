161ba5c5df1983873043dd6703218044df4cdf4c
DustyRoller <DustyRoller@github.com>
2022-07-29 23:43:39 +0100

    Improved unit test logic for checking coordinates

--- KakuroSolverUnitTests/ParserUnitTests.cs
+++ KakuroSolverUnitTests/ParserUnitTests.cs
@@ -69,6 +69,7 @@ namespace KakuroSolver.UnitTests
         public void Parser_ParsePuzzle_FailsWithPuzzleWithInvalidCharacters()
         {
             var sb = new StringBuilder();
+
             sb.AppendLine("|  x  |  x  |  x  |");
             sb.AppendLine("|  x  |?|  x  |");
 
@@ -94,71 +95,48 @@ namespace KakuroSolver.UnitTests
             Assert.AreEqual(5u, puzzle.Width);
             Assert.AreEqual(5u, puzzle.Height);
 
+            // Assert that the cell coordinates are correct.
+            var index = 0;
+
+            for (var y = 0u; y < puzzle.Height; ++y)
+            {
+                for (var x = 0u; x < puzzle.Width; ++x)
+                {
+                    Assert.AreEqual(new Coordinate(x, y), puzzle.Cells[index].Coordinate);
+
+                    index++;
+                }
+            }
+
             Assert.IsInstanceOf(typeof(BlankCell), puzzle.Cells[0]);
-
             Assert.AreEqual(17u, ((ClueCell)puzzle.Cells[1]).ColumnClue);
-            Assert.AreEqual(new Coordinate(1u, 0u), puzzle.Cells[1].Coordinate);
-
             Assert.AreEqual(24u, ((ClueCell)puzzle.Cells[2]).ColumnClue);
-            Assert.AreEqual(new Coordinate(2u, 0u), puzzle.Cells[2].Coordinate);
-
             Assert.IsInstanceOf(typeof(BlankCell), puzzle.Cells[3]);
             Assert.IsInstanceOf(typeof(BlankCell), puzzle.Cells[4]);
 
             Assert.AreEqual(16u, ((ClueCell)puzzle.Cells[5]).RowClue);
-            Assert.AreEqual(new Coordinate(0u, 1u), puzzle.Cells[5].Coordinate);
-
             Assert.IsInstanceOf(typeof(PuzzleCell), puzzle.Cells[6]);
-            Assert.AreEqual(new Coordinate(1u, 1u), puzzle.Cells[6].Coordinate);
-
             Assert.IsInstanceOf(typeof(PuzzleCell), puzzle.Cells[7]);
-            Assert.AreEqual(new Coordinate(2u, 1u), puzzle.Cells[7].Coordinate);
-
             Assert.AreEqual(20u, ((ClueCell)puzzle.Cells[8]).ColumnClue);
-            Assert.AreEqual(new Coordinate(3u, 1u), puzzle.Cells[8].Coordinate);
-
             Assert.IsInstanceOf(typeof(BlankCell), puzzle.Cells[9]);
 
             Assert.AreEqual(23u, ((ClueCell)puzzle.Cells[10]).RowClue);
-            Assert.AreEqual(new Coordinate(0u, 2u), puzzle.Cells[10].Coordinate);
-
             Assert.IsInstanceOf(typeof(PuzzleCell), puzzle.Cells[11]);
-            Assert.AreEqual(new Coordinate(1u, 2u), puzzle.Cells[11].Coordinate);
-
             Assert.IsInstanceOf(typeof(PuzzleCell), puzzle.Cells[12]);
-            Assert.AreEqual(new Coordinate(2u, 2u), puzzle.Cells[12].Coordinate);
-
             Assert.IsInstanceOf(typeof(PuzzleCell), puzzle.Cells[13]);
-            Assert.AreEqual(new Coordinate(3u, 2u), puzzle.Cells[13].Coordinate);
-
             Assert.AreEqual(15u, ((ClueCell)puzzle.Cells[14]).ColumnClue);
-            Assert.AreEqual(new Coordinate(4u, 2u), puzzle.Cells[14].Coordinate);
 
             Assert.IsInstanceOf(typeof(BlankCell), puzzle.Cells[15]);
-
             Assert.AreEqual(23u, ((ClueCell)puzzle.Cells[16]).RowClue);
-            Assert.AreEqual(new Coordinate(1u, 3u), puzzle.Cells[16].Coordinate);
-
             Assert.IsInstanceOf(typeof(PuzzleCell), puzzle.Cells[17]);
-            Assert.AreEqual(new Coordinate(2u, 3u), puzzle.Cells[17].Coordinate);
-
             Assert.IsInstanceOf(typeof(PuzzleCell), puzzle.Cells[18]);
-            Assert.AreEqual(new Coordinate(3u, 3u), puzzle.Cells[18].Coordinate);
-
             Assert.IsInstanceOf(typeof(PuzzleCell), puzzle.Cells[19]);
-            Assert.AreEqual(new Coordinate(4u, 3u), puzzle.Cells[19].Coordinate);
 
             Assert.IsInstanceOf(typeof(BlankCell), puzzle.Cells[20]);
             Assert.IsInstanceOf(typeof(BlankCell), puzzle.Cells[21]);
-
             Assert.AreEqual(14u, ((ClueCell)puzzle.Cells[22]).RowClue);
-            Assert.AreEqual(new Coordinate(2u, 4u), puzzle.Cells[22].Coordinate);
-
             Assert.IsInstanceOf(typeof(PuzzleCell), puzzle.Cells[13]);
-            Assert.AreEqual(new Coordinate(3u, 4u), puzzle.Cells[23].Coordinate);
-
             Assert.IsInstanceOf(typeof(PuzzleCell), puzzle.Cells[24]);
-            Assert.AreEqual(new Coordinate(4u, 4u), puzzle.Cells[24].Coordinate);
         }
     }
 }
